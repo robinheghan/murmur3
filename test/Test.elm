5cdf2d9501729c7498d4dca17b40d2bddd7b14b8
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2016-07-15 20:27:20 +0200

    Simplified test code

--- test/Test.elm
+++ test/Test.elm
@@ -4,20 +4,20 @@ import ElmTest exposing (..)
 import Murmur3
 
 
-hashFn : String -> Int
+hashFn : a -> Int
 hashFn =
-    Murmur3.hashString 63476
+    toString >> Murmur3.hashString 63476
 
 
 tests : Test
 tests =
     suite "Hashing"
-        [ test "int" <| assertEqual 1992578978 <| hashFn <| toString -102433675
-        , test "float" <| assertEqual 335970363 <| hashFn <| toString 4.32
-        , test "rec" <| assertEqual 3455049611 <| hashFn <| toString { name = "Robin", age = "27", male = True }
-        , test "tuple" <| assertEqual 12752532 <| hashFn <| toString ( "Robin", 27, True )
-        , test "ls" <| assertEqual 4202619459 <| hashFn <| toString [1..6]
-        , test "bool" <| assertEqual 108766572 <| hashFn <| toString False
+        [ test "int" <| assertEqual 1992578978 <| hashFn -102433675
+        , test "float" <| assertEqual 335970363 <| hashFn 4.32
+        , test "rec" <| assertEqual 3455049611 <| hashFn { name = "Robin", age = "27", male = True }
+        , test "tuple" <| assertEqual 12752532 <| hashFn ( "Robin", 27, True )
+        , test "ls" <| assertEqual 4202619459 <| hashFn [1..6]
+        , test "bool" <| assertEqual 108766572 <| hashFn False
         ]
 
 
